67bf1d74bb4152ec36ef3c981e7eaa96fb66de48
valentin <valentinpinte@hotmail.fr>
2019-10-17 15:11:45 +0200

    Separation

--- part1_lexic/lexic.c
+++ part1_lexic/lexic.c
@@ -1,1 +1,14 @@
 #include "lexic.h"
+
+void format_text(char *origine)
+{
+    int count = 0;
+
+    for (int i = 0; origine[i]; i++)
+    {
+        origine[i] = tolower(origine[i]);
+        if (origine[i] != ' ')
+            origine[count++] = origine[i];
+    }
+    origine[count] = '\0';
+}
--- part1_lexic/test.c
+++ part1_lexic/test.c
@@ -1,25 +1,12 @@
-#include <ctype.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-
-void trim(char *origine)
-{
-    int count = 0;
-
-    for (int i = 0; origine[i]; i++)
-    {
-        origine[i] = tolower(origine[i]);
-        if (origine[i] != ' ')
-            origine[count++] = origine[i];
-    }
-    origine[count] = '\0';
-}
+#include "lexic.h"
 
 int main(void)
 {
     char test[] = "test coucou tetetEEEEEetetet ewqewqe ";
-    trim(test);
+    format_text(test);
     printf("origine %s\n", test);
     return 0;
 }
