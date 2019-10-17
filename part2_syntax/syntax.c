ddf8157c0f31c2aa02ff4a0b80f6f57d6f8da52d
Nath Pop <nath.popc@gmail.com>
2019-10-17 14:10:17 +0900

    Update

--- part2_syntax/syntax.c
+++ part2_syntax/syntax.c
@@ -1,1 +1,20 @@
-#include "syntax.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "../jeton.h"
+#include "./syntax.h"
+
+//On recoit en entrée un tableau de typejeton
+
+Arbre analyse_syntaxe(typejeton* tableau_jeton, int taille)
+{
+    int i_jeton=0;
+    Arbre arbre = (Arbre)malloc(sizeof(Node));
+    arbre->pjeton_preced = NULL;
+    arbre->pjeton_suiv   = NULL;
+
+    //REEL,OPERATEUR,FONCTION,ERREUR,FIN,PAR_OUV,PAR_FERM,VARIABLE,BAR_OUV,BAR_FER,ABSOLU
+
+
+    return arbre;
+}
+
---
+++ part2_syntax/syntax.h
@@ -0,0 +1,7 @@
+#ifndef SYNTAX_H_INCLUDED
+#define SYNTAX_H_INCLUDED
+
+
+Arbre analyse_syntaxe(typejeton* tableau_jeton, int taille);
+
+#endif // SYNTAX_H_INCLUDED
