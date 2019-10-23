d4e657de6bf5dd1a7089249811f0f5ae081f637b
Lucas HADJERES <lucas.hadjeres@b12n14lnx.imtld.local>
2019-10-23 08:55:20 +0200

    Test recup

---
+++ part1_lexic/test.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "lexic.h"
+
+int main(void)
+{
+    char test[] = "sin(220.203 + 20)";
+    format_text(test);
+    decoupe_saisie(test);
+    //printf("origine %s\n", test);
+    return 0;
+}
